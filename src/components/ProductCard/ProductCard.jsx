908a6d463b9d29c92471db37570b6b41e26189c1
Khayreey <ahmed5ayreey@gmail.com>
2023-06-07 23:51:52 +0300

    update

--- src/components/ProductCard/ProductCard.jsx
+++ src/components/ProductCard/ProductCard.jsx
@@ -5,8 +5,8 @@ import AltImage from '../../assets/alt.png'
 const ProductCard = ({imgSrc , title , amountToDisplay}) => {
   const amontToDis = amountToDisplay === 0 ? 12 : amountToDisplay===2 ? 6 : 4
   return (
-    <Col   className={`col-${amontToDis} d-flex justify-content-center `}>
-        <div className='shadow-sm bg-light  rounded  m-2 d-flex flex-column justify-content-around align-items-around' style={{ width : '80%' , height : '250px'}} >
+    <Col  className={`col-${amontToDis} d-flex justify-content-center `}>
+        <div className='shadow-sm bg-light  rounded  m-2 d-flex flex-column justify-content-around align-items-around' style={{ maxWidth: '300px' , width : '80%' , height : '150px'}} >
            <img src={imgSrc} 
            onError={({ currentTarget }) => {
             currentTarget.onerror = null; // prevents looping
